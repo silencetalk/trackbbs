8662a1379897f9689fb56b3a15bde4ea2e146b8c
vagrant <vagrant@homestead>
2019-06-29 05:16:43 +0000

    生成用户认证代码

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'TrackBBS') - Track BBS</title>
+  <title>@yield('title', 'TrackBBS') - Track BBS</title>
 
-    <!-- Styles -->
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+  <!-- Styles -->
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
 </head>
 
 <body>
 <div id="app" class="{{ route_class() }}-page">
 
-    @include('layouts._header')
+  @include('layouts._header')
 
-    <div class="container">
+  <div class="container">
 
-        @include('shared._messages')
+    @include('shared._messages')
 
-        @yield('content')
+    @yield('content')
 
-    </div>
+  </div>
 
-    @include('layouts._footer')
+  @include('layouts._footer')
 </div>
 
 <!-- Scripts -->
